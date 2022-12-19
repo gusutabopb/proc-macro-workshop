8bb569e8e914c3fda275d970f8e112e184de6041
Gustavo Bezerra <gusutabopb@gmail.com>
2022-12-19 23:42:07 +0900

    debug: test 03

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -2,9 +2,9 @@ use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
 use syn::spanned::Spanned;
-use syn::{Data, DataStruct, Fields, FieldsNamed};
+use syn::{Attribute, Data, DataStruct, Field, Fields, FieldsNamed, PathSegment};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     match derive_impl(input) {
         Ok(t) => t.into(),
@@ -22,10 +22,19 @@ fn derive_impl(input: TokenStream) -> Result<TokenStream2, syn::Error> {
         return Err(syn::Error::new(input.span(), "expected struct with fields"));
     };
 
-    let fields = fields.iter().map(|f| {
-        let name = &f.ident;
-        quote!(.field(stringify!(#name), &self.#name))
-    });
+    let fields: Vec<_> = fields
+        .iter()
+        .map(|field| {
+            let Field { ident, attrs, .. } = field;
+            match attrs.first() {
+                None => Ok(quote!(.field(stringify!(#ident), &self.#ident))),
+                Some(attr) => {
+                    let _fmt = parse_attr(attr)?;
+                    Ok(quote!(.field(stringify!(#ident), &format_args!(#_fmt, &self.#ident))))
+                }
+            }
+        })
+        .collect::<Result<Vec<_>, syn::Error>>()?;
 
     Ok(quote!(
         impl ::std::fmt::Debug for #name {
@@ -36,4 +45,20 @@ fn derive_impl(input: TokenStream) -> Result<TokenStream2, syn::Error> {
             }
         }
     ))
+}
+
+fn parse_attr(attr: &Attribute) -> Result<String, syn::Error> {
+    use syn::{Lit, Meta::NameValue, MetaNameValue};
+    let meta = attr.parse_meta()?;
+    let err = Err(syn::Error::new(meta.span(), "failed to parse attribute"));
+    let NameValue(MetaNameValue { path, lit, ..}) = &meta else {
+        return err;
+    };
+    let Some( PathSegment { ident, .. } ) = path.segments.first() else {
+        return err;
+    };
+    match (ident.to_string().as_str(), lit) {
+        ("debug", Lit::Str(s)) => Ok(s.value()),
+        _ => err,
+    }
 }
